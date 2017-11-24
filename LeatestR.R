39637914a0f645f04725b5aaefd5abc48a7ad115
Koellmer-L <33145533+Koellmer-L@users.noreply.github.com>
2017-11-24 14:09:44 +0100

    Update LeatestR.R
    NA steht jetzt in Tabellen

--- LeatestR.R
+++ LeatestR.R
@@ -2,7 +2,7 @@ data <- read.csv("https://raw.githubusercontent.com/joachim-gassen/oss_mame/mast
 data
 
 data1 <- read.csv("https://raw.githubusercontent.com/joachim-gassen/oss_mame/master/raw_data/exam_data/mame_exam_1_raw_data.csv?token=AfnCve1TUZJ4q0XJhsOIpGEGxoo7hDz6ks5aIFy2wA%3D%3D" , TRUE, ";")
-data2 <- read.csv("https://raw.githubusercontent.com/joachim-gassen/oss_mame/master/raw_data/exam_data/mame_exam_2_raw_data.csv?token=AfnCvVn82FSUp6QX3ENAyMfxr7KIz3TZks5aIF27wA%3D%3D" , TRUE, ";")
+data2 <- read.csv("https://raw.githubusercontent.com/joachim-gassen/oss_mame/master/raw_data/exam_data/mame_exam_2_raw_data.csv?token=AfnCvVn82FSUp6QX3ENAyMfxr7KIz3TZks5aIF27wA%3D%3D" , sep=";", dec= ",", na.strings="", nrows = 100)
 data2
 getwd()
 saveRDS(data1, "data1.RDS")
